e1b31fb51cd9a2da2fce133c62d183493f4ab75a
Adam Rais <adamgoodapp@gmail.com>
2014-03-13 21:32:34 +0000

    New file

---
+++ robo2.rb
@@ -0,0 +1,20 @@
+require 'wiringpi'
+
+# initialize the GPIO port:
+gpio = WiringPi::GPIO.new
+# name the pins, for easier reference:
+left = 25
+
+# initialize the pin functions:
+gpio.mode left, INPUT
+
+# loop on all the pins, when the button pin has 
+# 0 value (button is pressed) the led will turn
+# on.
+loop do
+  state = gpio.readAll
+  if state[left] == 0
+    puts "yay left"
+  end    
+  sleep 0.3
+end
