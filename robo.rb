7e5bfde64ae61d74a2af9620ed5a5faacdf3821b
Adam Rais <adamgoodapp@gmail.com>
2014-03-16 04:35:05 +0000

    d

--- robo.rb
+++ robo.rb
@@ -5,60 +5,10 @@ include PiPiper
 
 ra = RoboticArm.new
 
-left = 2
-right = 3
-up = 12
-down = 11
-
-watch :pin => left do
+watch :pin => 25 do
   if value == 0
   	ra.base.left 0.7
   end
 end
 
-watch :pin => right do
-  puts "Pin changed from #{last_value} to #{value}"
-  if value == 0
-  	ra.base.right 0.7
-  end
-end
-
-# watch :pin => up do
-#   puts "Pin changed from #{last_value} to #{value}"
-#   if value == 0
-#   	ra.wrist.up 0.3
-#   end
-# end
-
-watch :pin => down do
-  puts "Pin changed from #{last_value} to #{value}"
-  if value == 0 && value == 1
-  	ra.stop
-  elsif value != 1 && value !=0
-  	ra.wrist.down 0.3
-  end
-end
-
-#Or
-
-# after :pin => 25, :goes => :low do
-#   puts "Button pressed"
-# end
-
 PiPiper.wait
-
-# while 1 > 0
-# 	case a 
-# 		when "\e[D"
-# 			puts "left"
-# 		when "\e[C"
-# 			puts "right"
-# 		when "\e[B"
-# 			puts "down"
-# 		when "\e[A"
-# 			puts "up"
-# 	end
-# end
-
-# ra.shoulder.up 0.5
-# ra.base.right 0.7
