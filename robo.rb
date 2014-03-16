81c2ff117f659690bac26edc243ea3169cd21e2f
Adam Rais <adamgoodapp@gmail.com>
2014-03-16 15:34:17 +0000

    s

--- robo.rb
+++ robo.rb
@@ -4,8 +4,12 @@ include PiPiper
 
 # ra = RoboticArm.new
 
-after :pin => 25, :goes => :high do
-  puts "#{:pin}"
+watch :pin => 25 do
+  if value == 0 || value == 1
+  	puts "i shouldnt be moving"
+  else 
+  	puts "turn left!"
+  end
 end
 
 PiPiper.wait
